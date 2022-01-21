5ebafb004339a5e65295766d85f8411908b77431
kumarparth380 <kumarparth380@gmail.com>
2022-01-21 10:16:58 +0530

    added meta tag component

---
+++ components/MetaData/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+
+const MetaData = () => {
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta
+        name="description"
+        content="Hey there, I'm Parth a JavaScript developer with more than 3.2 years of software development experience and skills in full-stack development with technologies like Nodejs, Express, GraphQL, React, Angular and have also made some mobile apps using react-native. I have also experience in Google Firebase(GCP) and AWS. "
+      />
+      <meta
+        name="keywords"
+        content="parth, kumarparth380, kumar parth, Parth, Kumar Parth Flexsin, Kumar Parth Nickelfox, Kumar Parth Mobilloitte, Kumar Parth VBSPU, Kumar Parth UNSIET, Purvanchal, Noida, Delhi, Varanasi, India"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#000" />
+      <meta property="og:title" content="Kumar Parth" />
+      <meta property="og:type" content="website" />
+      <meta name="author" content="Kumar Parth" />
+      <meta property="og:image:height" content="800" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="480" />
+      <meta
+        property="og:description"
+        content="Hey there, I'm Parth a JavaScript developer with more than 3.2 years of software development experience and skills in full-stack development with technologies like Nodejs, Express, GraphQL, React, Angular and have also made some mobile apps using react-native. I have also experience in Google Firebase(GCP) and AWS. "
+      />
+      <meta property="og:site_name" content="Kumar Parth" />
+      <meta name="robots" content="index, follow" />
+      <title>Kumar Parth</title>
+      <link rel="icon" sizes="96x96" type="image/png" href="/favicon.png" />
+    </Head>
+  )
+}
+
+export default MetaData
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,23 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
+import MetaData from '../components/MetaData'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-black p-10 pt-20 min-h-screen'>
-      <Head>
-        <title>Kumar Parth</title>
-        <meta name="description" content="Kumar Parth" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="bg-black p-10 pt-20 min-h-screen">
+      <MetaData />
 
-      <main className='w-2/3 mx-auto'>
-        <h1 className='text-gray-100 text-5xl'>Hi, I&apos;m Parth</h1>
-        <p className='text-gray-300 text-lg mt-10 w-2/3'> I am a JavaScript developer with more than 3.2 years of software development experience and skills in full-stack development with technologies like Nodejs, Express, GraphQL, React, Angular and have also made some mobile apps using react-native. I have also experience in Google Firebase(GCP) and AWS. I like to play video games and listen music.
+      <main className="w-2/3 mx-auto">
+        <h1 className="text-gray-100 text-5xl">Hi, I&apos;m Parth</h1>
+        <p className="text-gray-300 text-lg mt-10 w-2/3">
+          {' '}
+          I am a JavaScript developer with more than 3.2 years of software
+          development experience and skills in full-stack development with
+          technologies like Nodejs, Express, GraphQL, React, Angular and have
+          also made some mobile apps using react-native. I have also experience
+          in Google Firebase(GCP) and AWS. I like to play video games and listen
+          music.
         </p>
       </main>
 
@@ -22,8 +25,7 @@ const Home: NextPage = () => {
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
