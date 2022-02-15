ebc7a200f4d26d3557e723115e1f4c2f77086a01
kumarparth380 <kumarparth380@gmail.com>
2022-02-15 22:58:38 +0530

    update home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import HitCounter from '../components/HitsCounter'
 import MetaData from '../components/MetaData'
 import Skills from '../components/Skills'
 import Technologies from '../components/Technologies'
+import parthPic from '../public/parth.jpeg'
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,23 @@ const Home: NextPage = () => {
       <MetaData />
 
       <main className="container md:w-2/3 mx-auto">
+        <div className="flex flex-col h-screen items-center justify-center">
+          <Image
+            height={160}
+            width={160}
+            className="self-center rounded-full"
+            objectFit="cover"
+            placeholder="blur"
+            alt="my-pic"
+            src={parthPic}
+          />
+          <h1 className="text-gray-100 text-4xl font-semibold mt-6 my-4">
+            Kumar Parth
+          </h1>
+          <h2 className="text-gray-100 text-2xl my-2 break-normal">
+            Software Engineer
+          </h2>
+        </div>
         <h1 className="text-gray-100 text-5xl">Hi, I&apos;m Parth</h1>
         <p className="text-gray-300 text-lg mt-10">
           {' '}
@@ -26,13 +44,6 @@ const Home: NextPage = () => {
       </main>
 
       <footer className="flex align-center justify-between">
-        <a
-          className="text-gray-300 bottom-0 md:mr-10"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer">
-          Made with NextJS and tailwindcss{' '}
-        </a>
         <HitCounter />
       </footer>
     </div>
